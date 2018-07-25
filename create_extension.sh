526934fbee9e48f5383fba765bd6f072bd363dc2
truongduyng <truongduyng@gmail.com>
2018-07-25 15:03:59 +0700

    amend it to fit our usage

--- create_extension.sh
+++ create_extension.sh
@@ -1,11 +1,6 @@
-# Because both template1 and the user postgres database have already been created,
-# we need to create the hstore extension in template1 and then recreate the postgres database.
-#
-# Running CREATE EXTENSION in both template1 and postgres can lead to
-# the extensions having different eid's.
-gosu postgres psql --dbname template1 <<EOSQL
-    CREATE EXTENSION hstore;
-    DROP DATABASE $POSTGRES_USER;
-    CREATE DATABASE $POSTGRES_USER TEMPLATE template1;
-EOSQL
+#!/bin/bash
+set -e
 
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE EXTENSION hstore;
+EOSQL
